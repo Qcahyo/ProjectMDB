bf1db3d49aac176a64287f5735587cf9919f5356
qcw <qcahyo63@gmail.com>
2019-11-15 01:45:49 +0700

    Detail kantor bagian cv tlpn,email dan petunjuk arah done

--- app/src/main/java/com/training/mdb/DetailKantorActivity.java
+++ app/src/main/java/com/training/mdb/DetailKantorActivity.java
@@ -3,9 +3,11 @@ package com.training.mdb;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -82,7 +84,7 @@ public class DetailKantorActivity extends AppCompatActivity implements OnMapRead
 		cvEmail.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-
+				sendEmail();
 			}
 		});
 
@@ -104,6 +106,29 @@ public class DetailKantorActivity extends AppCompatActivity implements OnMapRead
 			}
 		});
 
+	}
+
+	@SuppressLint("LongLogTag")
+	protected void sendEmail() {
+		Log.i("Send email", "");
+		String[] TO = {""};
+		String[] CC = {""};
+		Intent emailIntent = new Intent(Intent.ACTION_SEND);
+
+		emailIntent.setData(Uri.parse("mailto:"));
+		emailIntent.setType("text/plain");
+		emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+		emailIntent.putExtra(Intent.EXTRA_CC, CC);
+		emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Your subject");
+		emailIntent.putExtra(Intent.EXTRA_TEXT, "Email message goes here");
+
+		try {
+			startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+			finish();
+			Log.i("Finished sending email...", "");
+		} catch (android.content.ActivityNotFoundException ex) {
+			Toast.makeText(DetailKantorActivity.this, "There is no email client installed.", Toast.LENGTH_SHORT).show();
+		}
 	}
 
 	@Override
