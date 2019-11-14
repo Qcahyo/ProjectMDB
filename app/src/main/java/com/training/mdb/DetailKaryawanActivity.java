0b38557ff07c591d3b2554079c163ddebbdc7021
Andrew-Hardianto <57761087+Andrew-Hardianto@users.noreply.github.com>
2019-11-14 21:52:12 +0700

    Update DetailKaryawanActivity.java

--- app/src/main/java/com/training/mdb/DetailKaryawanActivity.java
+++ app/src/main/java/com/training/mdb/DetailKaryawanActivity.java
@@ -14,7 +14,7 @@ public class DetailKaryawanActivity extends AppCompatActivity {
 
 	private static final String TAG = "DetailKaryawanActivity";
 
-	TextView tvDetailNama, tvDetailNik, tvDetailAlamat, tvDetailGender;
+	TextView tvDetailNama, tvDetailNik, tvDetailAlamat, tvDetailGender, tv_tempat, tv_tanggal, tv_agama, tv_gol, tv_status, tv_kewarganegaraan;
 	CircleImageView imgFotoProfil;
 
 	@Override
@@ -27,6 +27,12 @@ public class DetailKaryawanActivity extends AppCompatActivity {
 		tvDetailAlamat = findViewById(R.id.tvDetailAlamat);
 		tvDetailGender = findViewById(R.id.tvDetailGender);
 		imgFotoProfil = findViewById(R.id.imgFotoProfil);
+        tv_agama = findViewById(R.id.tv_agama);
+        tv_tempat = findViewById(R.id.tv_tempat);
+        tv_tanggal = findViewById(R.id.tv_tgl);
+        tv_gol = findViewById(R.id.tv_gol);
+        tv_status = findViewById(R.id.tv_status);
+        tv_kewarganegaraan = findViewById(R.id.tv_kewarganegaraan);
 
 		Intent intent = getIntent();
 		KaryawanConstruct karyawanConstruct = intent.getParcelableExtra("detailKaryawan");
@@ -51,6 +57,12 @@ public class DetailKaryawanActivity extends AppCompatActivity {
 		tvDetailAlamat.setText(Alamat);
 		tvDetailGender.setText(Gender);
 		Picasso.with(this).load(fotoKaryawan).into(imgFotoProfil);
+		tv_agama.setText(Agama);
+		tv_tanggal.setText(tanggalLahir);
+		tv_tempat.setText(tempatLahir);
+		tv_gol.setText(golonganDarah);
+		tv_status.setText(statusPerkawinan);
+		tv_kewarganegaraan.setText(Kewarganegaraan);
 
 	}
 }
